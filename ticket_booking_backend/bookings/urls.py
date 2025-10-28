e447b4baf0fa2995ab5ae81d510be1e7c6b5d102
Emmanuel Kipngeno <142774827+ManuuWatson@users.noreply.github.com>
2025-10-28 20:54:52 +0300

    First Commit

--- ticket_booking_backend/bookings/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Temporary placeholder until we add booking endpoints
-    path('', views.placeholder, name='bookings-home'),
-]
--- ticket_booking_backend/events/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import EventView
-
-urlpatterns = [
-    path('', EventView.as_view(), name='event-list'),              # /api/events/
-    path('<int:event_id>/', EventView.as_view(), name='event-detail'),  # /api/events/1/
-]
